9c868a36ffc8d003fd9f7c621f62787a356e87a4
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-04 20:18:03 -0300

    Create 1-export_to_CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    Using what you did in the task #0, extend your
+    Python script to export data in the CSV format.
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import sys
+    from urllib import request
+
+    user_id = sys.argv[1]
+    user = None
+    tasks = None
+    completed_tasks = None
+
+    user_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                               "/users/{}".format(user_id))
+    user = json.loads(user_url.read().decode("utf-8"))
+
+    tasks_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                                "/todos?userId={}".format(user_id))
+    tasks = json.loads(tasks_url.read().decode("utf-8"))
+    completed_tasks = list(filter(lambda task: task["completed"], tasks))
+
+    tasks_file = open("{}.csv".format(user["id"]), "w")
+
+    for task in completed_tasks:
+        tasks_file.write("\"{}\",\"{}\","
+                         "\"{}\",\"{}\"\n".format(user["id"],
+                                                  user["name"],
+                                                  task["completed"],
+                                                  task["title"]))
+
+    tasks_file.close()
