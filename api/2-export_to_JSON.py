8e20d506badb3671e7b00a7ef100345829e68dd4
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-04 20:48:52 -0300

    Create 2-export_to_JSON script.

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Using what you did in the task #0, extend your Python
+    script to export data in the JSON format.
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import sys
+    from urllib import request
+
+    user_id = sys.argv[1]
+    user = None
+    tasks = None
+    user_json = None
+
+    user_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                               "/users/{}".format(user_id))
+    user = json.loads(user_url.read().decode("utf-8"))
+
+    tasks_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                                "/todos?userId={}".format(user_id))
+    tasks = json.loads(tasks_url.read().decode("utf-8"))
+
+    tasks_file = open("{}.json".format(user["id"]), "w")
+
+    user_json = {
+        "{}".format(user["id"]): tasks
+    }
+    tasks_file.write(json.dumps(user_json))
+    tasks_file.close()
