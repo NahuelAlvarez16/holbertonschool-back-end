3825b8784f51dfcfd6f4c9f0a60265e6d83e6d4b
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-12-04 20:04:12 -0300

    Fix pep8 in 0-gather_data_from_an_API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,22 +6,26 @@
 
 
 if __name__ == "__main__":
-    import sys
     import json
     from urllib import request
+    import sys
 
     user_id = sys.argv[1]
     user = None
     tasks = None
     completed_tasks = None
 
-    user_url = request.urlopen("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    user_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                               "/users/{}".format(user_id))
     user = json.loads(user_url.read().decode("utf-8"))
 
-    tasks_url = request.urlopen("https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id))
+    tasks_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                                "/todos?userId={}".format(user_id))
     tasks = json.loads(tasks_url.read().decode("utf-8"))
     completed_tasks = list(filter(lambda task: task["completed"], tasks))
 
-    print("Employee {} is done with tasks({}/{}):".format(user["name"], len(completed_tasks), len(tasks)))
+    print("Employee {} is done with tasks({}/{}):".format(user["name"],
+                                                          len(completed_tasks),
+                                                          len(tasks)))
     for task in completed_tasks:
         print("\t {}".format(task['title']))
